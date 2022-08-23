97c555a77c8aaa963f889759661529731e03ca81
Tornike Takvarelia <ttakvarelia@beyondtrust.com>
2022-08-23 19:32:59 +0400

    change unit tests for renamed endpoints

--- src/test/java/com/crypto/recommendation/RecommendationApplicationTests.java
+++ src/test/java/com/crypto/recommendation/RecommendationApplicationTests.java
@@ -32,7 +32,7 @@ class RecommendationApplicationTests {
 		List<String> prices = new ArrayList<>(
 				Arrays.asList("BTC,0.434121","LTC,0.465184","XRP,0.517857","DOGE,0.461538","ETH,0.638381"));
 		Mockito.when(priceRepository.findNormalizedRange()).thenReturn(new ArrayList<String>(prices));
-		mockMvc.perform(get("/prices/normalRange/"))
+		mockMvc.perform(get("/recommendation-service/normal-range/"))
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("$", hasSize(5)));
 
@@ -44,7 +44,7 @@ class RecommendationApplicationTests {
 				Arrays.asList("BTC,47722.66,33276.59,2022-01-31 15:00:00.0,2022-01-01 08:00:00.0,1",
 						"LTC,151.50,103.40,2022-01-31 21:00:00.0,2022-01-01 10:00:00.0,1"));
 		Mockito.when(priceRepository.getMonthlyStats(1)).thenReturn(prices);
-		mockMvc.perform(get("/prices/monthly/1"))
+		mockMvc.perform(get("/recommendation-service/monthly/1"))
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("$", hasSize(2)));
 	}
@@ -54,7 +54,7 @@ class RecommendationApplicationTests {
 		List<String> prices = new ArrayList<>(
 				Arrays.asList("BTC,47722.66,33276.59,2022-02-01 00:00:00.0,2022-01-01 08:00:00.0"));
 		Mockito.when(priceRepository.getMonthlyStatsByCrypto("BTC")).thenReturn(prices);
-		mockMvc.perform(get("/prices/name/BTC"))
+		mockMvc.perform(get("/recommendation-service/crypto/BTC"))
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("$", hasSize(1)));
 	}
